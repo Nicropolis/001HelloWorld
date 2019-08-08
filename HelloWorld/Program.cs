6d8374ae25e0b6a08a08f2b26353d2902e38752e
Nicropolis <mavorozhtsov@gmail.com>
2019-08-08 15:59:17 +0300

    comment added

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,6 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //we will try second branch
             Console.WriteLine("Hello world, Misha");
         }
     }
